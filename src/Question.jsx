8a1f90dc38839a5f22465980de13123a472f5b2b
roj <kimroj2004@gmail.com>
2024-11-19 08:18:54 +0800

    first commit

---
+++ src/Question.jsx
@@ -0,0 +1,112 @@
+export const questions = [
+  {
+    question_aid: 1,
+    question_question: "Q1 Pogi ba si bryan?",
+    choices: [
+      {
+        choice: "ewan",
+        isCorrect: false,
+      },
+      {
+        choice: "baka",
+        isCorrect: false,
+      },
+      {
+        choice: "oo ata",
+        isCorrect: true,
+      },
+      {
+        choice: "sabe",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    question_aid: 2,
+    question_question: "Q2 Ano ang nauna manok o kambing?",
+    choices: [
+      {
+        choice: "manok",
+        isCorrect: false,
+      },
+      {
+        choice: "baka",
+        isCorrect: false,
+      },
+      {
+        choice: "aso",
+        isCorrect: false,
+      },
+      {
+        choice: "itlog",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    question_aid: 3,
+    question_question: "Q3 Bakit?",
+    choices: [
+      {
+        choice: "oo",
+        isCorrect: false,
+      },
+      {
+        choice: "hinde",
+        isCorrect: false,
+      },
+      {
+        choice: "ewan",
+        isCorrect: false,
+      },
+      {
+        choice: "sino",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    question_aid: 4,
+    question_question: "Q4 Tatama ba ako sa Jueteng?",
+    choices: [
+      {
+        choice: "pag tumaya",
+        isCorrect: false,
+      },
+      {
+        choice: "pag nabangga mo ang magjujueteng",
+        isCorrect: true,
+      },
+      {
+        choice: "hinde",
+        isCorrect: false,
+      },
+      {
+        choice: "no",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    question_aid: 5,
+    question_question: "Q5 Masama ba pag bad?",
+    choices: [
+      {
+        choice: "no",
+        isCorrect: false,
+      },
+      {
+        choice: "i dont know",
+        isCorrect: false,
+      },
+      {
+        choice: "yes",
+        isCorrect: true,
+      },
+      {
+        choice: "yes yes yow",
+        isCorrect: false,
+      },
+    ],
+  },
+];
